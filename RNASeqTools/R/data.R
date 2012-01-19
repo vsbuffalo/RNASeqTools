3783f0c6d6392b2cbed8ff9f88a283a2a13eab8a
Vince Buffalo <vsbuffalo@gmail.com>
2012-01-19 11:13:25 -0800

    initial import

---
+++ RNASeqTools/R/data.R
@@ -0,0 +1,31 @@
+joinColumns <-
+function(x, groups, fun=rowSums) {
+  # the result will have the same number of rows as x, and the same
+  # number of columns as unique integers in groups.
+  out <- vector('list', length(unique(groups)))
+  
+  for (level in unique(groups)) {
+    ci <- which(groups == level)
+    out[[level]] <- fun(x[, ci])
+  }
+  d <- as.data.frame(do.call(cbind, out))
+  rownames(d) <- rownames(x)
+  d
+}
+sumTrancripts2Genes <-
+function(x, mapping) {
+  if (!(is.data.frame(x) && is.data.frame(mapping)))
+    stop("x and mapping must be dataframes.")
+  if (ncol(mapping) != 2)
+    stop("The mapping dataframe must be two columns.")
+
+  # reorder mapping file such that it corresponds to rownames of x (transcripts)
+  ii <- match(rownames(x), mapping[, 2])
+  not.found <- sum(is.na(ii))
+
+  if (not.found > 0)
+    warning(sprintf("%d transcripts were not found in the mapping file. If this number is high, check the the second column is the transcript IDs!", not.found))
+  
+  tmp <- split(x, list(genes=mapping[ii, 1]))
+  do.call(rbind, lapply(tmp, colSums))
+}
---
+++ RNASeqTools/R/plots.R
@@ -0,0 +1,23 @@
+mdsPlot <-
+function(x, conds=NULL, cex=1, ...) {
+  d <- dist(x)
+  
+  mds.fit <- cmdscale(d, eig=TRUE, k=2)
+  
+  mds.d <- data.frame(x1=mds.fit$points[, 1],
+                      x2=mds.fit$points[, 2],
+                      labels=rownames(x))
+  if (!is.null(conds))
+    mds.d$treatment <- as.factor(conds)
+  
+  if (!is.null(conds)) {
+    p <- xyplot(x2 ~ x1, group=treatment, data=mds.d, panel=function(x, y, ..., groups, subscripts) {
+      panel.text(x, y, mds.d$labels[subscripts], cex=cex, col=trellis.par.get()$superpose.line$col[groups])
+    }, ...)
+  } else {
+    p <- xyplot(x2 ~ x1, data=mds.d, panel=function(x, y, ..., groups, subscripts) {
+      panel.text(x, y, mds.d$labels[subscripts], cex=cex)
+    }, ...)
+  }
+  return(p)
+}
